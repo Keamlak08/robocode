ab9fe72f37e5be0245982a068b582c9fefada05f
Chris Jones <keamlakk@gmail.com>
2024-05-29 23:52:29 -0400

    tried to find a counter to 'walls' cuz i was losing to it a bunch

--- robots/keamlakkebede/keamlakKebede.java
+++ robots/keamlakkebede/keamlakKebede.java
@@ -1,9 +1,8 @@
 package keamlakkebede;
 
-import static robocode.util.Utils.normalRelativeAngleDegrees;
-
 import java.awt.*;
 import robocode.Robot;
+import robocode.util.Utils;
 import robocode.*;
 //import java.util.List;
 //import java.util.*;
@@ -20,24 +19,16 @@ public class keamlakKebede extends Robot{
         
         movingForward = true;
         setAdjustGunForRobotTurn(true);
+        
+        
         while(true){
-            turnGunRight(360);
             moveRandomly();
-
-
-            /* 
-            before moveRandom():
-            ahead(Math.random() * 300);
-            movingForward = true;
-            turnRight(Math.random() * 180);
-            back(Math.random() * 100);
-            turnLeft(Math.random() * 200);
-            */
-            
-
+            turnGunRight(360);
         }
 
     }
+
+
 
     private void moveRandomly(){
         if (movingForward){
@@ -47,17 +38,18 @@ public class keamlakKebede extends Robot{
         }
         movingForward = !movingForward; //makes it so that it changes where it goes
 
-        turnRight(Math.random() * 180 - 90); // turns 45 degree right or left, found this on yt
+        turnRight(Math.random() * 90 - 45); // turns 45 degree right or left, found this on yt
     }
 
     public void onScannedRobot(ScannedRobotEvent e){
         
         double lockOn = getHeading() + e.getBearing() - getRadarHeading();
         double copy = getPerepheral(lockOn);
-        
-        if (Math.abs(copy) > Rules.GUN_TURN_RATE){
-        turnRight(lockOn);
-        }
+
+        //track fire aim:
+        //double getPosition = getHeading() + e.getBearing();
+        //double predictAim = getPerepheral(getPosition - getGunHeading());
+
         turnGunRight(copy);
         
         //took this from "Fire" sample robot :)
@@ -73,52 +65,74 @@ public class keamlakKebede extends Robot{
             fire(Math.min(400 / e.getDistance(), 6));
         }
         
+
         /* 
-        //took this from "Track Fire"
-        double absoluteBearing = getHeading() + e.getBearing();
-        double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
-        if (Math.abs(bearingFromGun) <= 3){
-            turnGunRight(bearingFromGun);
 
+        ------REST OF THIS COMMENT IS FROM INTERNET FOR EXPIREMNTATION PURPOSES-----
+        double bulletPower = Math.min(3.0, getEnergy());
+        double myX = getX();
+        double myY = getY();
+        double enemyX = myX + e.getDistance() * Math.sin(Math.toRadians(getHeading() + e.getBearing()));
+        double enemyY = myY + e.getDistance() * Math.cos(Math.toRadians(getHeading() + e.getBearing()));
+        double enemyHeading = e.getHeading();
+        double enemyVelocity = e.getVelocity();
 
-            if (getGunHeat() == 0){
-                fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
+        double deltaTime = 0;
+        double predictedX = enemyX;
+        double predictedY = enemyY;
+
+        while ((++deltaTime) * (20 - 3 * bulletPower) < Math.hypot(predictedX - myX, predictedY - myY)) {
+            predictedX += Math.sin(Math.toRadians(enemyHeading)) * enemyVelocity;
+            predictedY += Math.cos(Math.toRadians(enemyHeading)) * enemyVelocity;
+
+            if(predictedX < 18.0 || predictedY < 18.0 || predictedX > getBattleFieldWidth() - 18.0 || predictedY > getBattleFieldHeight() - 18.0) {
+                predictedX = Math.min(Math.max(18.0, predictedX), getBattleFieldWidth() - 18.0);
+                predictedY = Math.min(Math.max(18.0, predictedY), getBattleFieldHeight() - 18.0);
+                break;
             }
-        } else {
-            turnGunRight(bearingFromGun);
+        }
+
+        double theta = Math.toDegrees(Math.atan2(predictedX - myX, predictedY - myY));
+        double gunTurn = getPerepheral(theta - getGunHeading());
+
+        turnGunRight(gunTurn);
+        if(getGunHeat() == 0 && Math.abs(gunTurn) < 10){
+            fire(bulletPower);
         }
         */
-
-        //if the robot is far ahead it aims a bit ahead of it
-        //if (e.getDistance() > 50){
-        //    lockOn += 1;
-        //}else if(e.getDistance() < 50){
-        //    lockOn = getHeading() + e.getBearing() - getRadarHeading();
-        //}
-
-        //turnRight(lockOn);
-        //turnGunRight(lockOn);
     }
+
+
+
+
+
 
     public void onHitWall(HitWallEvent event) {
         reverseDirection();
     }
 
-    public void onBulletHit(BulletHitEvent event){
+    public void onHitByBullet(HitByBulletEvent event){
+        turnRight(Math.random() * 50 - 25);
+        reverseDirection();
+    }
+
+
+    public void onHitRobot(HitRobotEvent event){
+        turnRight(90);
         reverseDirection();
     }
 
     private void reverseDirection(){
         if (movingForward){
-            back(Math.random() * 200 + 100);
+            back(Math.random() * 100 + 50);
             movingForward = false;
         } else {
-            ahead(Math.random() * 200 + 100);
+            ahead(Math.random() * 100 + 50);
             movingForward = true;
         }
     }
 
-    //this gets the perepheral and finds how far to the left or right the robot was
+    //this gets the perepheral and finds how far to the left or right the robot's angle is
     //I used chatgpt to help with the logic of this (I couldn't find how to do this myself)
     private double getPerepheral(double angle){
         while (angle > 180){
